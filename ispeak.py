84cee81d9feb654314e535b61c62985310a87a1d
yud <145757584+yudstrz@users.noreply.github.com>
2025-09-24 08:18:35 +0700

    Update ispeak.py

--- ispeak.py
+++ ispeak.py
@@ -21,6 +21,9 @@
 import os, sys, re, tempfile, warnings, pickle, h5py
 import numpy as np
 import pandas as pd
+import subprocess
+# Install spaCy model
+subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
 
 # Streamlit
 import streamlit as st
@@ -1430,4 +1433,5 @@ streamlit run ispeak_improved.py
         """)
 
 if __name__ == "__main__":
+
     main()
